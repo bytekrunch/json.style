0b42fbe87e12d74f6eb7eda6dd1d567881b835a3
Sreeram Venkitesh <sreeram.venkitesh@bigbinary.com>
2022-12-21 11:19:58 +0530

    Added changes from review

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,22 +1,16 @@
 import { useState, useEffect } from 'react';
+import { FOOTER_MESSAGES } from './constants';
 
 const Footer = () => {
-  let messages = [
-    "Cars Are Now Designed to Keep Dogs Safe on Hot Days",
-    "Penguins Propose and Stick Together for Life",
-    "Baby Sloths Are Addicted to Cuddling",
-    "A Group of Bunnies Is Called a Fluffle",
-  ];
-
-  const [randomNumber, setRandomNumber] = useState(undefined);
+  const [randomNumber, setRandomNumber] = useState(0);
 
   useEffect(() => {
-    setRandomNumber(Math.floor(Math.random() * messages.length));
+    setRandomNumber(Math.floor(Math.random() * FOOTER_MESSAGES.length));
   }, []);
 
   return (
     <div className="absolute bottom-0 flex flex-col space-y-0 w-screen text-center h-10 px-10 my-5">
-      <span className='italic text-slate-500'>"{messages[randomNumber]}"</span>
+      <span className="italic text-slate-500">"{FOOTER_MESSAGES[randomNumber]}"</span>
       <span text-black>Made with 💔 </span>
     </div>
   )
---
+++ components/constants.js
@@ -0,0 +1,6 @@
+export const FOOTER_MESSAGES = [
+  "Cars Are Now Designed to Keep Dogs Safe on Hot Days",
+  "Penguins Propose and Stick Together for Life",
+  "Baby Sloths Are Addicted to Cuddling",
+  "A Group of Bunnies Is Called a Fluffle",
+];
