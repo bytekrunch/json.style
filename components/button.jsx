32ff1de8d7605f3c8d2f926f83cad961c18acbf2
Shreya Kurian <shreyakurian99@gmail.com>
2022-12-21 14:25:27 +0530

    Added common button component

--- components/button.jsx
+++ components/button.jsx
@@ -1,9 +1,37 @@
-const Button = () => {
+import { motion } from "framer-motion";
+import classnames from "classnames";
+
+const Button = ({
+  className = "",
+  style = "secondary",
+  size = "medium",
+  onClick = () => {},
+  disabled = false,
+  label = "Clicked",
+  ...otherProps
+}) => {
+  const handleClick = () => {
+    if (!disabled) onClick();
+  };
+
   return (
-    <button>
-      Click me
-    </button>
-  )
-}
+    <motion.button
+      onClick={handleClick}
+      disabled={disabled}
+      className={classnames("button", [className], {
+        "button--style-primary": style === "primary",
+        "button--style-secondary": style === "secondary",
+        "button--style-text": style === "text",
+        "button--size-small": size === "small",
+        "button--size-medium": size === "medium",
+        "button--size-large": size === "large",
+        disabled: disabled,
+      })}
+      {...otherProps}
+    >
+      {label && <span>{label}</span>}
+    </motion.button>
+  );
+};
 
 export default Button;
