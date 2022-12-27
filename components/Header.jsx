02a632eeb02aaea816f511ebb69dd13a65ca7b3c
Sreeram Venkitesh <sreeram.venkitesh@bigbinary.com>
2022-12-27 20:54:56 +0530

    Fixed header text size

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
 
   return (
     <div className="flex flex-row items-center justify-between h-10 px-10 mb-5">
-      <p className="h-min font-mono bg-gradient-to-br from-fuchsia-500 via-purple-600 to-rose-600 inline-block text-transparent bg-clip-text">&#123; json.style &#125;</p>
+      <p className="h-min font-mono text-lg bg-gradient-to-br from-fuchsia-500 via-purple-600 to-rose-600 inline-block text-transparent bg-clip-text">&#123; json.style &#125;</p>
       <Button
         size="medium"
         style="secondary"
