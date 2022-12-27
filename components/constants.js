2b478d450e33ec92f5c143f0ced0786e23c1f4e9
Shreya Kurian <shreyakurian99@gmail.com>
2022-12-27 16:56:16 +0530

    Added demo json and functionality to copy output json

--- components/constants.js
+++ components/constants.js
@@ -4,3 +4,26 @@ export const FOOTER_MESSAGES = [
   "Baby Sloths Are Addicted to Cuddling",
   "A Group of Bunnies Is Called a Fluffle",
 ];
+
+export const DEMO_JSON_STRING = {
+  glossary: {
+    title: "example glossary",
+    GlossDiv: {
+      title: "S",
+      GlossList: {
+        GlossEntry: {
+          ID: "SGML",
+          SortAs: "SGML",
+          GlossTerm: "Standard Generalized Markup Language",
+          Acronym: "SGML",
+          Abbrev: "ISO 8879:1986",
+          GlossDef: {
+            para: "A meta-markup language, used to create markup languages such as DocBook.",
+            GlossSeeAlso: ["GML", "XML"],
+          },
+          GlossSee: "markup",
+        },
+      },
+    },
+  },
+};
