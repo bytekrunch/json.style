5c1b8691bbde58d799ff5b99cf46b652da6b02df
Sreeram Venkitesh <sreeram.venkitesh@bigbinary.com>
2023-01-05 22:00:14 +0530

    Removed ;

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ const publicSans = Public_Sans({
 function MyApp({ Component, pageProps }) {
   return (
     <main className={publicSans.className}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </main>
   )
 }
